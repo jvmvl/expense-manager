121f8203977fcd20f554d5e7a0454cb8f88a004d
jvmvl <xspider7@hotmail.com>
2024-12-24 11:36:43 +0100

    Day 2: Learn about controllers and create your first controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExpenseController;
 
 Route::get('/', function () {
     return view('welcome', ['message' => 'Welcome to Expense Manager!']);
 });
+
+Route::get('/expenses', [ExpenseController::class, 'index']);
+Route::get('/expenses/create', [ExpenseController::class, 'create']);
+Route::post('/expenses', [ExpenseController::class, 'store']);
